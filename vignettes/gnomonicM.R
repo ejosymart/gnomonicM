f7216b418557ee5e4fb40545e9375730a0d8967b
josymar <ejosymart@gmail.com>
2020-05-24 18:12:57 -0600

    improveing vignettes

--- vignettes/gnomonicM.R
+++
@@ -1,7 +0,0 @@
-## ---- eval=FALSE--------------------------------------------------------------
-#  install.packages("gnomonicM")
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  # install.packages("devtools")
-#  devtools::install_github("ejosymart/gnomonicM")
-
